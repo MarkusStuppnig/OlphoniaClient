256ab18a6649f51f0447f25cacbcd80b4e07bb71
MarkusStuppnig <mstuppnig@student.tgm.ac.at>
2022-10-10 15:10:41 +0200

    Useable Workspace

--- script/index.js
+++ script/index.js
@@ -1,38 +1,47 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path').join(__dirname,'../');
 const url = require('url');
 
+let package_file = require('../package.json');
+let name = package_file.name_view;
+
 function createWindow () {
+
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    title: name,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
+
+  //Remove Menubar
+  win.setMenu(null);
+
   //load the index.html of the app.
-  console.log(__dirname);
   win.loadURL(url.format({
-    pathname : path.join(__dirname,'../index.html'),
+    pathname : path + 'index.html',
     protocol:'file',
     slashes:true
-  }))
-
+  }));
 }
+
 //call the creation function when app is done loading
 app.whenReady().then(createWindow)
 
 //this event is invoked when user is quitting the application
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
+
 //this event is used re-create window when user minimize the window and
 //maximize again
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
